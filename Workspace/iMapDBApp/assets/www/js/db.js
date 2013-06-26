e2c5cc4b2173c093d3061fe507023410bc9188d2
citrit <citrit2@gmail.com>
2013-06-25 21:12:37 -0400

    App init loads the database and preferences.

--- Workspace/iMapDBApp/assets/www/js/db.js
+++ Workspace/iMapDBApp/assets/www/js/db.js
@@ -5,102 +5,26 @@ var DBFuncs = {
 	// Application Constructor
     init: function() {
     	iMapApp.debugMsg("DB init");
-    	iMapDB = window.openDatabase("iMapDB", "1.0", "iMap Invasive Species DB", 1000000);
-    	iMapDB.transaction(DBFuncs.checkForDataFiles, DBFuncs.errorCB);
+    	iMapDB = window.sqlitePlugin.openDatabase({  
+    		   name : "iMapInvasives"  
+    	 });
     	iMapDB.transaction(DBFuncs.checkForUpdates, DBFuncs.errorCB);
     },
     // check if the database has been loaded.
     checkForUpdates: function(tx, results) {
     	iMapApp.debugMsg("Check for server updates...");
+    	tx.executeSql("SELECT count(*) from imiadmin_state_species_list", [], function(tx, results) {
+    		if (results.rows.length > 0) {
+    			iMapApp.debugMsg("iMapInvasives DB all set.");
+    		}
+    	}, DBFuncs.errorCB);
     },
     // check if the database has been loaded.
     checkForDataFiles: function(tx, results) {
     	tx.executeSql("DROP TABLE IF EXISTS imiadmin_state_species_list");
     	tx.executeSql("SELECT name FROM sqlite_master WHERE type='table' AND name='imiadmin_state_species_list'", [], DBFuncs.firstTimeDBLoad, DBFuncs.errorCB);
     },
-	// Application Constructor
-    firstTimeDBLoad: function(tx, results) {
-    	iMapApp.debugMsg($.toJSON(results.rows));
-    	
-    	if (results.rows.length == 0) {
-        	iMapApp.debugMsg("DB time to create the tables...");
-    		DBFuncs.loadSQLFile(tx, "res/state_species_list.sql");
-    	}
-    	else {
-        	iMapApp.debugMsg("DB is initialized. Nothinh to do here");
-    	}
-    },
-    // Load the state species list
-	loadSQLFile: function(tx, sqlFile) {
-    	var request = new XMLHttpRequest();
-    	iMapApp.debugMsg("Loading: " + sqlFile);
-        request.open("GET", sqlFile, false);
-        var rcnt = 0;
-        request.onreadystatechange = function(){
-        	iMapApp.debugMsg("state = " + request.readyState);
-            if (request.readyState == 4) {
-                if (request.status == 200 || request.status == 0) {
-                    var sqlQueries = request.responseText.split("\n");
-                    var curTable = "";
-                    $.each(sqlQueries, function(index, val) {
-						if (val.substr(0,6) == "CREATE") {
-							//iMapApp.debugMsg("create: " + $.toJSON(val));
-							if (val.substr(0,12) == "CREATE TABLE") {
-								curTable = val.split(" ")[2];
-							}
-							tx.executeSql(val);
-							rcnt++;
-						}
-						else {
-							if (val.length > 0) {
-								DBFuncs.execInsertCmd(tx,val, curTable);
-								rcnt++;
-							}
-						}
-                    });
-                    iMapApp.debugMsg("Executed [" + rcnt + "] sql statements");
-                }
-            }
-        }
-        request.send();
-    },
-    // Parse Date
-    dateFromString: function(str) {
-    	var m = str.match(/(\d+)-(\d+)-(\d+)\s+(\d+):(\d+):(\d+)\.(\d+)/);
-    	var ret = Number.NaN;
-    	if (m && m.length > 6) {
-    		ret = new Date(+m[1], +m[2] - 1, +m[3], +m[4], +m[5], +m[6] * 100);
-    	}
-    	return ret;
-    },
-    // Exec insert command
-    execInsertCmd: function(tx, val, intoTable) {
-    	//vals = val.replace(/	/g, ",");
-    	vals = val.split("	");
-    	var sqlCmd = "INSERT INTO " + intoTable + " VALUES(";
-    	$.each(vals, function(index, val) {
-    		if (!isNaN(parseInt(val)) || !isNaN(parseFloat(val))) {
-    			sqlCmd += val + ",";
-    		}
-    		else if (val == "t") {
-    			sqlCmd += "TRUE,";
-    		}
-    		else if (val == "f") {
-    			sqlCmd += "FALSE,";
-    		}
-    		else if (!isNaN(DBFuncs.dateFromString(val))){
-    			sqlCmd += "Date(" + val + "),";
-    		}
-    		else {
-    			sqlCmd += "\"" + val + "\",";
-    		}
-    		
-    	});
-    	//sqlCmd += vals
-    	sqlCmd = sqlCmd.substr(0,sqlCmd.length-1) + ")";
-    	iMapApp.debugMsg("execInserted: " + sqlCmd);
-    	tx.executeSql(sqlCmd, null, DBFuncs.errorCB);
-    },
+    // Exec a simple SQL command
     execSQL: function(sqlStr, callBackFunc) {
     	iMapDB.transaction(callBackFunc, DBFuncs.errorCB);
     },
--- Workspace/iMapDBApp/assets/www/js/index.js
+++ Workspace/iMapDBApp/assets/www/js/index.js
@@ -1,26 +1,7 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var debugOut = true;
 
 var iMapApp = {
-    
-	userParams: null,
+	
+	debugOut: true,
 	
 	// Application Constructor
     initialize: function() {
@@ -39,25 +20,8 @@ var iMapApp = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
     	console.log("onDeviceReady made it here.");
-    	iMapApp.loadParameters();
+    	iMapPrefs.init();
     	iMapApp.loadDatabase();
-    },
-    // load the parameters file.
-    loadParameters: function() {
-    	console.log('loading parameters');
-    	var request = new XMLHttpRequest();
-        request.open("GET", "res/parameters.json", false);
-        request.onreadystatechange = function(){
-        	iMapApp.debugMsg("state = " + request.readyState);
-            if (request.readyState == 4) {
-                if (request.status == 200 || request.status == 0) {
-                	iMapApp.debugMsg('read: ' + request.responseText);
-                	iMapApp.userParams = $.parseJSON(request.responseText);
-                	iMapApp.debugMsg('user params read: ' + $.toJSON(iMapApp.userParams));
-                }
-            }
-        }
-        request.send();
     },
     // Load the database.
     loadDatabase: function() {
@@ -65,7 +29,42 @@ var iMapApp = {
     },
     // Output debug messages.
     debugMsg: function(msg) {
-    	if (debugOut)
+    	if (iMapApp.debugOut)
     		console.log( msg );
     }
 };
+
+var iMapPrefs = {
+		params: {
+			Firstname: "",
+			Lastname: "",
+			Username: "",
+			Password: "",
+			Projects: [],
+			Plants: {
+				UseCommon: true,
+				UseScientific: true,
+				MyPlants: []
+			},
+			PictureSize: "Medium",
+		},
+		init: function() {
+			var parms = localStorage.getItem("userParams");
+	    	if (parms != null) {
+	    		iMapApp.debugMsg("found existing user Params: " + parms);
+	    		iMapPrefs.params = $.parseJSON(parms);
+	    	}
+	    	else {
+	    		iMapApp.debugMsg("need to init user Params");
+	    		localStorage.setItem("userParams", $.toJSON(iMapPrefs.params));
+	    	}
+		},
+		saveParams: function() {
+			iMapApp.debugMsg("iMapPrefs: saving user Params");
+    		localStorage.setItem("userParams", $.toJSON(iMapPrefs.params));
+		},
+		loadParams: function() {
+			iMapApp.debugMsg("iMapPrefs: loading user Params");
+			iMapPrefs.params = $.parseJSON(localStorage.getItem("userParams"));
+		}
+}
