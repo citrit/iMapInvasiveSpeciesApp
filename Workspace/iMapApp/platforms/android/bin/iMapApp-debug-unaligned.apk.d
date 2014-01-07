35e21acbf83f28e42ddec315254faf5bf5ca79ce
citrit <citrit2@gmail.com>
2014-01-06 21:26:57 -0500

    Latest changes for checkpoint of working code except for the upload.

--- Workspace/iMapApp/platforms/android/bin/iMapApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/200019935/git/iMapInvasiveSpeciesApp/Workspace/iMapApp/platforms/android/bin/iMapApp-debug-unaligned.apk : \
-/Users/200019935/git/iMapInvasiveSpeciesApp/Workspace/iMapApp/platforms/android/bin/iMapApp.ap_ \
-/Users/200019935/git/iMapInvasiveSpeciesApp/Workspace/iMapApp/platforms/android/bin/classes.dex \
-/Users/200019935/git/iMapInvasiveSpeciesApp/Workspace/iMapApp/platforms/android/libs/cordova-3.1.0.jar \
